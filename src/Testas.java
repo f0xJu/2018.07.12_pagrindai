25eb28b37e5fa81d12e4df6f2a01bdb3aea10757
f0xJu <deathas@gmail.com>
2018-07-23 01:52:04 +0300

    kazkas

--- src/Testas.java
+++ src/Testas.java
@@ -4,55 +4,40 @@ public class Testas {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Iveskite du skaicius ir veiksma tarp ju, pvz '20 + 20'");
+        System.out.println("Iveskite du skaicius ir veiksma tarp ju, pvz '2 + 2'");
 
         int numb1;
         int numb2;
-        int sum;
-        int dif;
+
         numb1 = scanner.nextInt();
         numb2 = scanner.nextInt();
-        sum = scanner.nextInt();
-        dif = scanner.nextInt();
-
-
 
 
         String line = "";
         while (!line.equals("quit")) {
-
-
             line = scanner.nextLine();
-
 
             String[] items = line.split(" ");
             if (items.length == 3) {
 
 
-                double result;
+
                 switch (items[1]) {
                     case "+":
-                        result = numb1 + numb2;
-                        System.out.println("Suma " + result);
+
+                        System.out.println("Suma " + sum(numb1, numb2));
                         break;
                     case "-":
-                        result = numb1 - numb2;
-                        System.out.println("Skyrtumas " + result);
+
+                        System.out.println("Skyrtumas " + dif(numb1, numb2));
                         break;
+
                     default:
-                        System.out.println("Nepavyko rasti");
+                        System.out.println("PFFFFFFFFFFFFFFFF");
                         break;
-
-
                 }
-
-
             }
-
-
         }
-
-
     }
     public static int sum (int numb1, int numb2){
         int sum = numb1 + numb2;
